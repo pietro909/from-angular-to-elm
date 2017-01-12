9f31040cdfd811f1457f169bed110db4f2d9ba7b
Pietro Grandi <pietro.grandi@evokegaming.com>
2017-01-12 13:52:35 +0100

    Upgrade gulp-elm to use 0.18 debugger

--- gulp/scripts.js
+++ gulp/scripts.js
@@ -3,12 +3,14 @@ const gulp = require('gulp');
 const elm  = require('gulp-elm');
 const minify = require('gulp-minify');
 
+const options = { debug: true, warn: true };
+
 gulp.task('elm-init', elm.init);
 
 gulp.task('elm-bundle', ['elm-init'], () =>
     // separate min
     gulp.src(config.source.elm)
-        .pipe(elm.bundle(`${config.output.elmApplicationName}.js`))
-        .pipe(minify())
-        .pipe(gulp.dest(config.public.js))
+      .pipe(elm.bundle(`${config.output.elmApplicationName}.js`, { debug: true }))
+      .pipe(minify())
+      .pipe(gulp.dest(config.public.js))
 );
